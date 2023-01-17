9077650033e9f07215ee24aa38256dc5a5a53241
DagmawiSolomon <dagmawis603@gmail.com>
2023-01-17 22:25:49 +0300

    add add.html to view and urls

--- joblisting/base/views.py
+++ joblisting/base/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 def home(request):
     context = {}
     return render(request, "home.html", context)
+
+
+def add(request):
+    context = {}
+    return render(request, "add.html", context)
